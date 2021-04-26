73d4953951b58766810d164617a29a2900649bfd
Allan Luchenitser <aluchenitser@gmail.com>
2021-04-25 21:08:05 -0400

    sliding CTA

--- js/ui.js
+++ js/ui.js
@@ -1,16 +1,40 @@
 const stickyElement = document.querySelector('.sticky');
+const sayHelloEl    = document.querySelector('.say-hello');
+const ctaTopEl      = document.querySelector('.cta-top');
+const ctaBottomEl     = document.querySelector('.cta-bottom');
+
+if(window.pageYOffset <= 10) {
+  ctaTopEl.classList.add('at-top')
+}
 
 window.addEventListener("scroll", e => {
   const isStuck = stickyElement.classList.contains('stuck');
+  const bottomOffset = 100;
 
   if(window.pageYOffset > 10 && !isStuck) {
     stickyElement.classList.add('stuck')
-  } else if (window.pageYOffset <= 10 && isStuck) {
-    stickyElement.classList.remove('stuck')
+    ctaTopEl.classList.remove('at-top');
   }
+  else if (window.pageYOffset <= 10 && isStuck) {
+    stickyElement.classList.remove('stuck')
+    ctaTopEl.classList.add('at-top');
+  }
+
+  let isOut = ctaBottomEl.classList.contains('at-bottom');
+  if(document.documentElement.offsetHeight - window.pageYOffset < window.innerHeight + bottomOffset && !isOut) {
+    ctaBottomEl.classList.add('at-bottom');
+  }
+  else if (document.documentElement.offsetHeight - window.pageYOffset >= window.innerHeight + bottomOffset && isOut) {
+    ctaBottomEl.classList.remove('at-bottom');
+  }
+
 })
 
 document.getElementById('menu-toggle').addEventListener('change', e => {
   document.body.classList.toggle('mobile-menu-open', e.target.checked);
   console.log('menu toggle?');
 })
+
+window.onload = function() {
+  document.body.classList.remove('preload')
+}
