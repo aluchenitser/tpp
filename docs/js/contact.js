b8427ec397520b1ce8d5a2a30868a8b0866d1b72
Allan <aluchenitser@gmail.com>
2021-08-30 05:49:57 -0400

    attempting lambda for contact page

--- docs/js/contact.js
+++ docs/js/contact.js
@@ -16,13 +16,13 @@ var app = new Vue({
         email: {
           value: '',
           required: true,
-          hint: 'Please enter a valid email!',
+          hint: 'Please enter an email!',
           type: 'email',
         },
         phone: {
           value: '',
+          hint: 'Please enter a valid phone number!',
           type: 'phone',
-          hint: 'Please enter a valid phone number!'
         },
         message: {
           value: '',
@@ -32,11 +32,28 @@ var app = new Vue({
       }
     },
     methods: {
-      success() {
-        console.log('cb success!');
+      async success() {
+        let response = await fetch('https://xc03285k6c.execute-api.us-east-1.amazonaws.com/contactPage', {
+          method: 'POST',
+          body: this.payload,
+        });
+
+        console.log('response');
+        console.log(response);
       },
       failure() {
         console.log('cb failure!');
       }
     },
+    computed: {
+      payload() {
+        let payload = {};
+
+        Object.keys(this.fields).map((element) => {
+          payload[element] = this.fields[element].value;
+        });
+
+        return payload;
+      }
+    }
   });
--- js/contact.js
+++ js/contact.js
@@ -21,6 +21,7 @@ var app = new Vue({
         },
         phone: {
           value: '',
+          hint: 'Please enter a valid phone number!',
           type: 'phone',
         },
         message: {
@@ -31,11 +32,28 @@ var app = new Vue({
       }
     },
     methods: {
-      success() {
-        console.log('cb success!');
+      async success() {
+        let response = await fetch('https://xc03285k6c.execute-api.us-east-1.amazonaws.com/contactPage', {
+          method: 'POST',
+          body: this.payload,
+        });
+
+        console.log('response');
+        console.log(response);
       },
       failure() {
         console.log('cb failure!');
       }
     },
+    computed: {
+      payload() {
+        let payload = {};
+
+        Object.keys(this.fields).map((element) => {
+          payload[element] = this.fields[element].value;
+        });
+
+        return payload;
+      }
+    }
   });
